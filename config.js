3e967d92b76c7ff4ce55483a023d6599ef87f17c
Glenn Lanzer <lanzertrey@gmail.com>
2018-06-29 20:29:21 -0500

    Init: config.js

---
+++ config.js
@@ -0,0 +1,6 @@
+module.exports = {
+  consumer_key: 'K0qiSQ2vPd3cQKt2toLlueGXk',
+  consumer_secret: 'mESMIqpfTgVpweLe2ej0hKREpjyL726FL7WOoaO6F4MD3LnJja',
+  access_token: '881051985021194241-7lZX9uTQghDJ4qTOSIbbq8vu52HmLwM',
+  access_token_secret: 'LtVUA5lLZFad5Odc6rDjDVgrZVbuEPDYN9DuQuXLGX9Pq'
+}
