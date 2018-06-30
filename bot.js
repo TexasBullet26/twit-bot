2aa5ea5ea3be90a1275991cf784ee67f5e9aaf94
Glenn Lanzer <lanzertrey@gmail.com>
2018-06-30 08:34:27 -0500

    Update (bot.js): get request

--- bot.js
+++ bot.js
@@ -1,6 +1,29 @@
+/** Log 'The bot is starting' to the console. */
 console.log('The bot is starting');
 
-var Twit = require('twit');  // Import the twit module.
+/** Import the twit module. */
+var Twit = require('twit');
 
-var config = require('./config');  // Authenticates using a different config.js file.
-var T = new Twit(config);  // Object that connects to Twitter.
+/** Authenticate by importing the `config.js` file with the API keys. */
+var config = require('./config');
+
+/** Object that connects to Twitter. */
+var T = new Twit(config);
+
+/** Search for 2 tweets with the word 'atmosphere'. */
+var params = {
+  q: 'atmosphere',
+  count: 2
+}
+
+/** Trigger the get and search Twitter for Tweets. */
+T.get('search/tweets', params, gotData);
+
+/** Function that will be triggered when the data has been returned from the API. */
+function gotData(err, data, response) {
+  var tweets = data.statuses;
+  for (var i = 0; i < tweets.length; i++) {
+    console.log(tweets[i].text);
+  }
+  console.log(data);
+}
